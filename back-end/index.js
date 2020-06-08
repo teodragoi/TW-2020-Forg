989c5e61af1ad92638f1d13b754d422e12a8a844
Teo Dragoi <teodorfdragoi@gmail.com>
2020-06-08 20:47:55 +0300

    Fixed connection

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,7 @@ const url = require('url');
 const database = require('./db/database');
 
 http.createServer(async (req, res) => {
+    await database.createConnection();
 
     const headers = {
         "Access-Control-Allow-Origin": "*",
@@ -19,19 +20,14 @@ http.createServer(async (req, res) => {
         case 'GET':
             const parsed = url.parse(req.url);
             const queryParams = queryString.parse(parsed.query);
-            await database.createConnection();
             if (req.url === '/Courses') {
                 const courses = await database.getCourses();
-                console.log
-
                 if (courses) {
                     res.writeHead(200, headers);
                     res.end(JSON.stringify(courses));
-                    database.closeConnection();
                 } else {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ 'err': 'Not found' }));
-                    database.closeConnection();
                 }
             } else if (req.url.includes('/Courses') && queryParams.courseId) {
                 const course = await database.getCourseById(queryParams.courseId);
@@ -39,21 +35,17 @@ http.createServer(async (req, res) => {
                 if (course) {
                     res.writeHead(200, headers);
                     res.end(JSON.stringify(course));
-                    database.closeConnection();
                 } else {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ 'err': 'Not found' }));
-                    database.closeConnection();
                 }
             }
             else {
                 res.writeHead(400, { 'Content-Type': 'appplication/json' });
                 res.end(JSON.stringify({ 'Error': 'Unmapped url' }));
-                database.closeConnection();
             }
             break;
         case 'POST':
-            await database.createConnection();
             let body = '';
             req.on('data', async chunk => {
                 body = JSON.parse(chunk.toString());
@@ -62,15 +54,12 @@ http.createServer(async (req, res) => {
                     if (addResult.insertedId) {
                         res.writeHead(201, headers);
                         res.end(JSON.stringify({ 'CourseId': addResult.insertedId }));
-                        database.closeConnection();
                     } else {
                         res.writeHead(500, { 'Content-Type': 'application/json' });
                         res.end(JSON.stringify({ 'Error': 'An error occured' }));
-                        database.closeConnection();
                     }
                 }
             });
     }
-
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
